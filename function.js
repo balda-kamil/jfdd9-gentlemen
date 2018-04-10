50a3f8f1c2af406331b724fa8043602900c040e3
Kuba <jkb.kam@gmail.com>
2018-04-11 01:05:52 +0200

    udało się ale trzeba dopracować

--- function.js
+++ function.js
@@ -32,17 +32,17 @@ function checkSlide(e) {
        sliderImages.forEach(sliderImage => {
 
            //half way through the image
-       const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+       const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.clientHeight / 2;
             //bottom of the image
-           console.log(slideInAt)
-       /*const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+       const imageBottom = sliderImage.offsetTop + sliderImage.clientHeight;
        const isHalfShown = slideInAt > sliderImage.offsetTop;
        const isNowScrolledPast = window.scrollY < imageBottom;
        if (isHalfShown && isNowScrolledPast) {
            sliderImage.classList.add('active');
        } else {
            sliderImage.classList.remove('active');
-       }*/
+       }
    })
 }
 window.addEventListener('scroll', debounce(checkSlide));
